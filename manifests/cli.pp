a6bca36e6f75bdb1bdd59871367fcc27da5b2a24
Peter Lieverdink <me@cafuego.net>
2015-08-28 11:08:49 +1000

    [#32843] Rename and move the credentials file, create a symlink.

--- manifests/cli.pp
+++ manifests/cli.pp
@@ -1,16 +1,28 @@
-# Cloudwatch Logs configuration.
+# Cloudwatch Logs credential file configuration.
 
-class cloudwatchlogs::cli {
+define cloudwatchlogs::cli (
+  $region = undef,
+) {
+
+  # Grab the credentials.
+  $aws_access_key_id     = hiera('cloudwatchlogs_key_id',     undef)
+  $aws_secret_access_key = hiera('cloudwatchlogs_access_key', undef)
 
   if $region and $aws_access_key_id and $aws_secret_access_key {
-    file { '/var/awslogs/etc/awscli.conf':
+    file { '/etc/awslogs/aws.conf':
       ensure  => 'file',
       owner   => 'root',
       group   => 'root',
       mode    => '0644',
-      require => File['/var/awslogs/etc'],
+      require => File['/etc/awslogs'],
       notify  => Service['awslogs'],
-      content => template('cloudwatchlogs/awscli.conf.erb'),
+      content => template('cloudwatchlogs/aws.conf.erb'),
+    }
+
+    file { '/var/awslogs/etc/aws.conf':
+      ensure  => 'link',
+      target  => '/etc/awslogs/aws.conf',
+      require => File['/etc/awslogs/aws.conf'],
     }
   }
 
